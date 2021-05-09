683299d7678b6cb17ea1819ca5ab7a3b4d58effc
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2021-05-09 16:16:01 +0300

    Basic prototype 1

--- Sample.jsx
+++ Sample.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import React, { useState, useEffect } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
-import './Sample.less';
+import { useWindowDimension } from "./useWindowDimension";
+import { useInterval } from "./useInterval";
+
+import "./Sample.less";
 
 const options = {
-  cMapUrl: 'cmaps/',
+  cMapUrl: "cmaps/",
   cMapPacked: true,
 };
 
 export default function Sample() {
-  const [file, setFile] = useState('./sample.pdf');
+  const [file, setFile] = useState("./sample.pdf");
   const [numPages, setNumPages] = useState(null);
 
   function onFileChange(event) {
@@ -21,39 +24,52 @@ export default function Sample() {
     setNumPages(nextNumPages);
   }
 
+  //screen size
+  const [width, height] = useWindowDimension();
+
+  //timer
+  const [counter, setCounter] = useState(1);
+
+  useInterval(() => {
+    setCounter(numPages > counter ? counter + 1 : 1);
+  }, 500);
+
   return (
     <div className="Example">
-      <header>
-        <h1>react-pdf sample page</h1>
-      </header>
+      {/* <header>
+        <h1>
+          {counter} {numPages}
+        </h1>
+      </header> */}
       <div className="Example__container">
-        <div className="Example__container__load">
+        {/* <div className="Example__container__load">
           <label htmlFor="file">Load from file:</label>
           {' '}
           <input
             onChange={onFileChange}
             type="file"
           />
-        </div>
-        <div className="Example__container__document">
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            {
-              Array.from(
-                new Array(numPages),
-                (el, index) => (
-                  <Page
-                    key={`page_${index + 1}`}
-                    pageNumber={index + 1}
-                  />
-                ),
-              )
-            }
-          </Document>
-        </div>
+        </div> */}
+      </div>
+      <div className="Example__container__document">
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {
+            // Array.from(
+            //   new Array(numPages),
+            //   (el, index) => (
+            //     <Page
+            //       key={`page_${index + 1}`}
+            //       pageNumber={index + 1}
+            //     />
+            //   ),
+            // )
+            <Page key={`page_${counter}`} pageNumber={counter} height={height} scale={0.9} />
+          }
+        </Document>
       </div>
     </div>
   );
